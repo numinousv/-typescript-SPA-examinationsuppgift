fbc867237302d9602d5cea60a887957a74facc8b
Rosanna <202148302+popsnoret@users.noreply.github.com>
2025-11-25 21:53:51 +0100

    added space in about

--- src/views/about/index.ts
+++ src/views/about/index.ts
@@ -1,21 +1,19 @@
 export default function about() {
   const about = document.createElement("div");
-  about.classList.add("about", "flex", "justify-center", "items-center", "min-h-screen");
+  about.classList.add("about", "flex", "justify-center", "items-start", "min-h-screen", "py-20");
 
   about.innerHTML = `
-    <div class="backdrop-blur-md bg-gray-900/60 p-10 rounded-4xl shadow-2xl max-w-2xl text-center">
+    <div class="backdrop-blur-md bg-gray-900/60 p-10 rounded-4xl shadow-2xl max-w-2xl text-center mt-10">
       <h1 class="font-[Great_Vibes] text-5xl mb-4">About the calendar</h1>
       <p class="text-lg text-gray-100 leading-relaxed mb-6">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, 
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+        ultricies nec, pellentesque eu, pretium quis, sem. 
       </p>
-   <img src="/anteaters-3.png" class="mx-auto rounded-2xl shadow-lg m-6">
-        <p class="text-lg text-gray-100 leading-relaxed mb-6">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
-        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, 
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-      </p>
+<img src="/anteaters-3.png" class="mx-auto rounded-2xl shadow-lg m-6 max-w-lg h-auto">
+
+
+    
     </div>
   `;
 
